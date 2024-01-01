b6b6965e7aa7d07b610007a0735104f0bfa737f5
leeseohyun <applemint2003@gmail.com>
2024-01-02 02:29:46 +0900

    유튜브 게임 개발 ing

---
+++ MiniGame-lsh/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public string targetScene;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(targetScene); //해당 씬으로 이동
+    }
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PlayerPrefs.SetInt("Name", 0);
+        PlayerPrefs.SetInt("Fame", 0);
+        PlayerPrefs.SetInt("Money", 0);
+    }
+
+}
